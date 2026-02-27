b4a7773ba7973200f04d81675cc2970f2faa4cf2
rony munich <ronymunich@gmail.com>
2026-02-27 05:52:39 +0000

    fix: change title layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from "@/firebase";
 
 export const metadata: Metadata = {
-  title: 'KCI Blood Drive Registration',
+  title: 'Donor Darah PT KCI',
   description: 'Donor Darah PT. Kereta Commuter Indonesia',
 };
 
